a12a4342a37b01f74a94ff8c66f0ac36d86879b8
Matthew <mduncan766@gmail.com>
2023-05-11 18:57:46 -0500

    adding env for security

--- Main/db/connection.js
+++ Main/db/connection.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql2");
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "Password1.",
-  database: "employees"
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+);
 
-connection.connect(function (err) {
-  if (err) throw err;
-});
-
-module.exports = connection;
+module.exports = sequelize;
